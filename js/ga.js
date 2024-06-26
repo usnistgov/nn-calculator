00490c9fc6376de2013f1f1b5f5b6644159c39f0
pnb <peter.bajcsy@nist.gov>
2024-06-26 16:48:34 -0400

    fixed the google analytics script that was mixing HTML and JavaScript

--- js/ga.js
+++ js/ga.js
@@ -1,5 +1,6 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-161627994-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-161627994-1">
+//<!-- Global site tag (gtag.js) - Google Analytics -->
+</script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
